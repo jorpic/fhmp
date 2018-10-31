fd86d6ec44fec96bef7fcd84b52444cd4818f774
Max Taldykin <jorpic@gmail.com>
2018-10-31 10:45:06 +0300

    Allow to sign in

---
+++ src/components/SignIn/index.js
@@ -0,0 +1,38 @@
+import { h, Component } from 'preact';
+import Card from 'preact-material-components/Card';
+import 'preact-material-components/Card/style.css';
+import 'preact-material-components/Button/style.css';
+import style from './style';
+import { auth, googleAuthProvider } from '../../firebase';
+
+
+export default class SignIn extends Component {
+  // returns promise with `{user}` as a result.
+  static check() {
+    return auth.getRedirectResult();
+  }
+
+
+  signIn() {
+    auth.signInWithRedirect(googleAuthProvider)
+  }
+
+
+  render() {
+    return (
+      <div class={`${style.signIn} page`}>
+        <h1>Sign In</h1>
+        <Card>
+          <div class={style.cardBody}>
+            You need to sign in to proceed.
+          </div>
+          <Card.Actions>
+            <Card.ActionButton onClick={this.signIn}>
+              Sign In
+            </Card.ActionButton>
+          </Card.Actions>
+        </Card>
+      </div>
+    );
+  }
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,35 +1,53 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
+import SignIn from './SignIn';
 import Header from './header';
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 import NotFound from '../routes/404';
-// import Home from 'async!../routes/home';
-// import Profile from 'async!../routes/profile';
 
 export default class App extends Component {
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.setState({
-			currentUrl: e.url
-		});
-	};
+  constructor(props) {
+    super(props);
+    SignIn.check()
+      .then(result => {
+        const signed = result && result.user;
+        this.setState({
+          user: signed ? result.user : null,
+          signIn: signed ? "signed" : "not signed"
+        });
+      })
+      .catch(() => this.setState({signIn: 'failed'}));
+  }
 
-	render() {
-		return (
-			<div id="app">
-				<Header selectedRoute={this.state.currentUrl}/>
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-          <NotFound default />
-				</Router>
-			</div>
-		);
-	}
+
+  handleRoute = e => {
+    this.setState({
+      currentUrl: e.url
+    });
+  };
+
+
+  render() {
+    return (
+      <div id="app">
+        <Header
+            selectedRoute={this.state.currentUrl}
+            message={this.state.signIn}
+        />
+        {this.state.signIn !== 'signed'
+          ? <SignIn />
+          : (
+            <Router onChange={this.handleRoute}>
+              <Home path="/" />
+              <Profile path="/profile/" user="me" />
+              <Profile path="/profile/:user" />
+              <NotFound default />
+            </Router>
+          )
+        }
+      </div>
+    );
+  }
 }
