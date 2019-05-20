debc72ef9314b0676e4768192d0571c01ff5a2a4
Max Taldykin <jorpic@gmail.com>
2019-05-20 21:26:17 +0300

    Generate random note id

--- src/db.js
+++ src/db.js
@@ -1,22 +1,30 @@
 import Dexie from "dexie";
 
-const DB_VERSION = 3;
+const DB_VERSION = 4;
 
 export default class Db {
   constructor() {
+    let ver = 0;
     this.idb = new Dexie("fhmp");
-    this.idb.version(1).stores({
+    this.idb.version(++ver).stores({
       Notes: "++id,createTime",
     });
-    this.idb.version(2).stores({
+    this.idb.version(++ver).stores({
       Notes: "++id,createTime",
       Drafts: "++id",
     });
-    this.idb.version(DB_VERSION).stores({
+    this.idb.version(++ver).stores({
       Notes: "++id,createTime",
       Drafts: "++id",
       Config: "id",
     });
+    this.idb.version(++ver).stores({
+      Config: "id",
+      Drafts: "++id",
+      Notes: "id,review_at",
+      Reviews: "",
+    });
+    console.assert(ver === DB_VERSION, "DB version mismatch");
   }
 
 
@@ -24,8 +32,15 @@ export default class Db {
 
 
   createNote = text => {
-    const createTime = new Date().toISOString();
-    return this.idb.Notes.add({createTime, text})
+    // We use timestamp with random suffix as unique note id.
+    // Couple of base-36 digits should be enough to prevent collisions
+    // with a dosen of devices.
+    const id = Date.now().toString(36)
+      + Math.random().toString(36).substring(1,4);
+    const created_at = new Date().toISOString();
+    // review this note as soon as possible
+    const review_at = created_at;
+    return this.idb.Notes.add({id, review_at, created_at, text})
       .then(() => this.idb.Drafts.clear());
   }
 
