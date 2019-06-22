754846306b0e6ff61cca48cff4602b8a706c911d
Max Taldykin <jorpic@gmail.com>
2019-06-22 13:43:28 +0300

    Simplify preact.config

--- preact.config.js
+++ preact.config.js
@@ -6,12 +6,12 @@
 export default (config, env, helpers) => {
   if(env.production) {
     config.output.publicPath = '/fhmp/';
-  }
 
-  // use the public path in your app as 'process.env.PUBLIC_PATH'
-  config.plugins.push(
-    new helpers.webpack.DefinePlugin({
-      'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/')
-    })
-  );
+    // use the public path in your app as 'process.env.PUBLIC_PATH'
+    config.plugins.push(
+      new helpers.webpack.DefinePlugin({
+        'process.env.PUBLIC_PATH': config.output.publicPath
+      })
+    );
+  }
 };
