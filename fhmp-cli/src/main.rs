a9e3fc3e7a3b0f91caa808a9e5a03a220a35c9b0
Max Taldykin <jorpic@gmail.com>
2022-04-27 18:53:56 +0300

    fhmp-cli: save notes to sqlite DB

--- fhmp-cli/src/main.rs
+++ fhmp-cli/src/main.rs
@@ -1,25 +1,26 @@
 use std::{io, env, path::PathBuf};
 
 use anyhow::{Context, Result};
-use config::{Config, ConfigError, File, FileFormat};
+use config::{Config, File, FileFormat};
 
 use chrono::{DateTime, Local};
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use serde_yaml::Value;
+
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 struct CliConfig {
     pub db_path: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Serialize,Deserialize)]
 struct Note {
     tags: String,
-    ctime: DateTime<Local>,
+    ctime: Option<DateTime<Local>>,
     card: Option<Vec<String>>,
     text: Option<String>
 }
-
 
 fn main() -> Result<()> {
     // read config
@@ -39,20 +40,43 @@ fn main() -> Result<()> {
     // read stdin
     let val: Value = serde_yaml::from_reader(io::stdin())
         .context("While reading YAML from stdin")?;
-    match val {
-        Value::Sequence(_) => {
-            let notes = Vec::<Note>::deserialize(val)
-                .context("Whlie parsing a sequence of notes from YAML")?;
-            println!("{:?}", notes);
-        },
-        Value::Mapping(_) => {
-            let note = Note::deserialize(val)
-                .context("While parsing a note from YAML")?;
-            println!("{:?}", note);
-        },
-        _ => {
-            anyhow::bail!("Object or sequence of objects expected.");
-        }
+    let notes = match val {
+        Value::Sequence(_) =>
+            Vec::<Note>::deserialize(val)
+                .context("Whlie parsing a sequence of notes from YAML")?,
+        Value::Mapping(_) => vec![
+            Note::deserialize(val)
+                .context("While parsing a note from YAML")?
+
+        ],
+        _ => anyhow::bail!("Object or sequence of objects expected.")
+    };
+
+    // init db
+    let db = sqlite::open(&cfg.db_path)?;
+    db.execute(
+        "create table if not exists notes(
+            uuid text not null,
+            ctime text not null,
+            json text not null);
+        "
+    )?;
+
+    // insert notes
+    let mut q = db.prepare("insert into notes values (?, ?, ?)")?;
+    for n in notes.iter() {
+        q.reset()?;
+        let id = Uuid::new_v4().to_string();
+        let ctime = match n.ctime {
+            Some(ctime) => ctime.to_rfc3339(),
+            None => Local::now().to_rfc3339(),
+        };
+        let data = serde_json::to_string(&n)?;
+
+        q.bind(1, id.as_str())?;
+        q.bind(2, ctime.as_str())?;
+        q.bind(3, data.as_str())?;
+        while let sqlite::State::Row = q.next()? { };
     }
     Ok(())
 }
