a777681216f459d61dce901650a59928e357a132
Max Taldykin <jorpic@gmail.com>
2022-04-28 10:21:18 +0300

    fhmp-cli: refactor

--- fhmp-cli/src/main.rs
+++ fhmp-cli/src/main.rs
@@ -23,37 +23,46 @@ struct Note {
 }
 
 fn main() -> Result<()> {
-    // read config
+    let cfg = read_config()
+        .context("Reading config")?;
+    let notes = read_notes(io::stdin())
+        .context("Reading notes from stdin")?;
+    let db = init_db(&cfg.db_path)
+        .context("Opening database file")?;
+    insert_notes(&db, &notes)
+}
+
+fn read_config() -> Result<CliConfig> {
     let home = env::var("HOME")
-        .context("While trying to get $HOME")?;
+        .context("Trying to get $HOME")?;
     let cfg_path: PathBuf = [&home, ".config", "fhmp", "config"].iter().collect();
-    let cfg: CliConfig = Config::builder()
+    Config::builder()
         .add_source(
             File::from(cfg_path)
                 .format(FileFormat::Toml)
                 .required(true))
         .build()?
         .try_deserialize()
-        .context("While parsing config")?;
+        .context("Parsing config")
+}
 
-
-    // read stdin
-    let val: Value = serde_yaml::from_reader(io::stdin())
-        .context("While reading YAML from stdin")?;
-    let notes = match val {
+fn read_notes<T: std::io::Read>(r: T) -> Result<Vec<Note>> {
+    let val: Value = serde_yaml::from_reader(r)?;
+    match val {
         Value::Sequence(_) =>
             Vec::<Note>::deserialize(val)
-                .context("Whlie parsing a sequence of notes from YAML")?,
-        Value::Mapping(_) => vec![
+                .context("Parsing a sequence of notes from YAML"),
+        Value::Mapping(_) => Ok(vec![
             Note::deserialize(val)
-                .context("While parsing a note from YAML")?
+                .context("Parsing a note from YAML")?
 
-        ],
+        ]),
         _ => anyhow::bail!("Object or sequence of objects expected.")
-    };
+    }
+}
 
-    // init db
-    let db = sqlite::open(&cfg.db_path)?;
+fn init_db(db_path: &str) -> Result<sqlite::Connection> {
+    let db = sqlite::open(db_path)?;
     db.execute(
         "create table if not exists notes(
             uuid text not null,
@@ -61,8 +70,14 @@ fn main() -> Result<()> {
             json text not null);
         "
     )?;
+    Ok(db)
+}
 
-    // insert notes
+
+fn insert_notes(
+    db: &sqlite::Connection,
+    notes: &[Note]
+) -> Result<()> {
     let mut q = db.prepare("insert into notes values (?, ?, ?)")?;
     for n in notes.iter() {
         q.reset()?;
