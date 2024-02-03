adcffd3fd456869de15b7dba341158fc28fa86b8
Max Taldykin <jorpic@gmail.com>
2024-02-03 14:58:06 +0400

    issues: ratatui hello world

--- fhmp-issues/src/main.rs
+++ fhmp-issues/src/main.rs
@@ -1,8 +1,49 @@
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal,
+    ExecutableCommand,
+};
+
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph,
+};
 use anyhow::Result;
+use std::io::stdout;
 
 mod config;
 
 fn main() -> Result<()> {
-    let conf = config::read_config()?;
+    // let _conf = config::read_config()?;
+
+    stdout().execute(terminal::EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+    let mut term = Terminal::new(CrosstermBackend::new(stdout()))?;
+    term.clear()?;
+
+    loop {
+         term.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                    .white()
+                    .on_blue(),
+                area,
+            );
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(160))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press
+                    && key.code == KeyCode::Char('q')
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(terminal::LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
